443bb26ee9de4fe149047b531e255ee34e74614b
filipion <filip.alexandru.ion@gmail.com>
2022-05-22 12:09:07 +0300

    Add graph weights.

--- graph.cpp
+++ graph.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <deque>
 #include <algorithm>
 #include <functional>
 using namespace std;
@@ -9,43 +10,53 @@ vector<vector<int>> SCC;
 int ANS;
 
 
+// Elements of the adjacency list we use. 
+// When edge(v, d) is in adj[u], v represents the outvertex and d the weight of the edge from u to v.
+struct Edge{
+	int vertex;
+	int weight;
+	Edge(int v, int w){
+		vertex = v, weight = w;
+	}
+};
+
+
+// This is a weighted graph class for the purpose of demonstrating graph algorithms. Weights can be left unspecified
+// since they initialize to 1 by default.
 class Graph{
 	private:
 		int size;
-		vector<vector<int>> adj;
+		vector<vector<Edge>> adj;
 
 		void dfs_helper(int v, vector<bool>& vis, function<void(int)>, function<void(int)>);
 		void bfs_helper(vector<bool>& vis, queue<int>& q);
 
 	public:
-		Graph(int sz) :size {sz}, adj {vector<vector<int>>(sz + 1)} {}
+		Graph(int sz) :size {sz}, adj {vector<vector<Edge>>(sz + 1)} {}
 
-		void add_edge(int v1, int v2);
 		void add_edge(int v1, int v2, int weight);
 		void print();
 
-		void dfs(const vector<int>& ordered_vertices, function<void(int)>, function<void(int)>);
+		void dfs(function<void(int)>, function<void(int)>);
 		void bfs(int start_vertex);
 
 		Graph reverse_graph();
-		void scc_helper(int v, vector<bool>& vis, vector<int>& order_for_scc);
-		vector<int> compute_order_scc();
 		void strongly_connected_components();
 
 };
 
 
-//Basic utilities, add edge, add weighted edge, print to stdout etc.
-void Graph::add_edge(int v1, int v2){
-	adj[v1].push_back(v2);
+//Basic utilities, add weighted edge, print to stdout etc.
+void Graph::add_edge(int v1, int v2, int weight=1){
+	adj[v1].push_back(Edge(v2, weight));
 }
 
 
 void Graph::print(){
 	for(int i=1; i <= size; ++i){
 		cout << i << ":";
-		for(auto u: adj[i])
-			cout << " " << u;
+		for(auto edge: adj[i])
+			cout << " " << edge.vertex;
 		cout << "\n";
 	}
 }
@@ -54,38 +65,23 @@ void Graph::print(){
 // The recursive dfs traversal algorithm.
 // Iterates through the vector of ordered vertices and calls dfs whenever it finds a new component. 
 // We initialize a vector vis of visits locally and pass it by reference to prevent useless copying.
-void Graph::dfs(const vector<int>& ordered_vertices = {}, function<void(int)> discovery_action = [](int){}, function<void(int)> finish_action = [](int){}){
-
+void Graph::dfs(function<void(int)> discovery_action = [](int){}, function<void(int)> finish_action = [](int){}){
 	vector<bool> vis(size);
 
-	if(!ordered_vertices.empty()){ // case of the second dfs of strongly connected components
-		int count_scc = 0;
-		for(auto v: ordered_vertices){
-			if(!vis[v]){
-				++count_scc;
-				SCC.push_back(vector<int> {});
-				dfs_helper(v, vis, discovery_action, finish_action);
-			}
-		}
-
-		ANS = count_scc;
-	}
-	else{ // default DFS
-		for(int v = 1; v <= size; ++v)
-			if(!vis[v])
-				dfs_helper(v, vis, discovery_action, finish_action);
-	}
+	for(int v = 1; v <= size; ++v)
+		if(!vis[v])
+			dfs_helper(v, vis, discovery_action, finish_action);
 }
 
 
-// Basic dfs recursive function.
-void Graph::dfs_helper(int v, vector<bool>& vis, function<void(int)> discovery_action, function<void(int)> finish_action){
+// Basic dfs recursive function. Supports passing functional arguments to execute at vertex discovery or finish.
+void Graph::dfs_helper(int v, vector<bool>& vis, function<void(int v)> discovery_action = [](int){}, function<void(int v)> finish_action = [](int){}){
 	vis[v] = 1;
 	discovery_action(v);
 
-	for(auto u: adj[v])
-		if(!vis[u])
-			dfs_helper(u, vis, discovery_action, finish_action);
+	for(auto e: adj[v])
+		if(!vis[e.vertex])
+			dfs_helper(e.vertex, vis, discovery_action, finish_action);
 	finish_action(v);
 }
 
@@ -106,10 +102,10 @@ void Graph::bfs(int start_vertex){
 void Graph::bfs_helper(vector<bool>& vis, queue<int>& q){
 	int v = q.front(); //starting vertex
 
-	for(auto u: adj[v]) //extend queue
-		if(!vis[u]){
-			q.push(u);
-			vis[u] = 1;
+	for(auto e: adj[v]) //extend queue
+		if(!vis[e.vertex]){
+			q.push(e.vertex);
+			vis[e.vertex] = 1;
 		}
 
 	q.pop(); 
@@ -123,20 +119,19 @@ void Graph::bfs_helper(vector<bool>& vis, queue<int>& q){
 // Required by Kosaraju's algorithm for SCC. This ordering satisfies the property that the first occurences
 // of all SCCs form a topologicaly sorted subsequence.
 void Graph::strongly_connected_components(){
-	vector<int> order_for_scc;
 	// Because DFS takes functions as parameters we do not have to implement it again
-	dfs({}, [](int){}, [&order_for_scc](int v){order_for_scc.push_back(v);}); 
-	reverse(order_for_scc.begin(), order_for_scc.end());
+	vector<int> order_for_scc;
+	dfs([](int){}, [&order_for_scc](int v){order_for_scc.push_back(v);});
 
 	Graph g_reversed = this->reverse_graph();
-
+	
 	vector<bool> vis(size);
-
-	for(auto v: order_for_scc){
+	for(auto it = order_for_scc.rbegin(); it != order_for_scc.rend(); ++it){
+		int v = *it;
 		if(!vis[v]){
 			// we DFS traverse again. Each call of dfs fills out a SCC
 			SCC.push_back(vector<int> {});
-			g_reversed.dfs_helper(v, vis, [](int v){SCC.back().push_back(v);}, [](int v){}); 
+			g_reversed.dfs_helper(v, vis, [](int v){SCC.back().push_back(v);}); 
 		}
 	}
 }
@@ -147,8 +142,8 @@ Graph Graph::reverse_graph(){
 	Graph rg(size);
 
 	for(int v = 1; v <= size; ++v)
-		for(auto u: adj[v])
-			rg.add_edge(u, v);
+		for(auto e: adj[v])
+			rg.add_edge(e.vertex, v);
 
 	return rg;
 }
