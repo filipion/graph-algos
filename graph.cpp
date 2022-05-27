16db8ce557781dd700ae45c118d067ac92c5605d
filipion <filip.alexandru.ion@gmail.com>
2022-05-27 11:56:07 +0300

    Add Johnson's algorithm

--- graph.cpp
+++ graph.cpp
@@ -10,10 +10,9 @@
 #include <cassert>
 
 using namespace std;
-vector<vector<int>> SCC;
-const int infty = 2*1e9;
+const int infty = 2*1e5;
+
 const int max_matrix = 31;
-int DEBUG;
 using Adj_Matrix = array<array<int, max_matrix>, max_matrix>;
 
 // Elements of the adjacency list we use. 
@@ -36,19 +35,21 @@ struct SymmetricEdge{
 };
 
 
-// Weighted graph class for the purpose of demonstrating graph algorithms
+// Weighted (ordered) graph
 class Graph{
 	private:
 		int size;
 		vector<vector<Edge>> adj;
 		vector<vector<SymmetricEdge>> sym_adj;
 
-		void dfs_helper(int v, vector<bool>& vis, function<void(int)>, function<void(int)>);
-		void bfs_helper(vector<bool>& vis, queue<int>& q);
+		void dfs_recursive(int v, vector<bool>& vis, function<void(int)>, function<void(int)>);
+		void bfs_recursive(vector<bool>& vis, queue<int>& q);
 
 	public:
 		Graph(int sz) :size {sz}, adj {vector<vector<Edge>>(sz + 1)}, sym_adj {vector<vector<SymmetricEdge>>(sz + 1)} {}
 
+		void push_vertex();
+		void pop_vertex();
 		void add_edge(int v1, int v2, int weight);
 		void add_symmetric_edge(int v1, int v2, int id, int weight);
 		void print();
@@ -56,8 +57,8 @@ class Graph{
 		void dfs(function<void(int)>, function<void(int)>);
 		void bfs(int start_vertex);
 
-		Graph reverse_graph();
-		void strongly_connected_components();
+		Graph reversed_graph();
+		vector<vector<int>> strongly_connected_components();
 
 		vector<int> minimal_spanning_tree(int source);
 
@@ -65,14 +66,18 @@ class Graph{
 		vector<int> shortest_paths_bellman_ford(int source);
 
 		int** all_shortest_paths(int** adj_matrix);
+		vector<vector<int>> all_shortest_paths_johnsons();
 
 		vector<int> eulerian_cycle(int number_edges);
 		void euler_helper(int node, vector<int>& vis, vector<int>& cycle);
 };
 
 
-//Basic utilities, add weighted edge, print to stdout etc.
+// Basic graph algorithms and manipulating the data structure:
+
+
 void Graph::add_edge(int v1, int v2, int weight=1){
+	// Adds an edge from v1 to v2 in the graph with weight "weight"
 	adj[v1].push_back(Edge(v2, weight));
 }
 
@@ -86,56 +91,75 @@ void Graph::add_symmetric_edge(int from, int to, int id, int weight=1){
 
 
 void Graph::print(){
+	cout << "Graph on " << size << " vertices with edges:";
+
 	for(int i=1; i <= size; ++i){
 		cout << i << ":";
 		for(auto edge: adj[i])
-			printf("(%d, %d) ", edge.vertex, edge.weight);
+			printf("(to: %d, weight: %d) ", edge.vertex, edge.weight);
 		cout << "\n";
 	}
 }
 
 
-// The recursive dfs traversal algorithm.
-// Iterates through the vector of ordered vertices and calls dfs whenever it finds a new component. 
-// We initialize a vector vis of visits locally and pass it by reference to prevent useless copying.
+void Graph::push_vertex(){
+	vector<Edge> neighbor_list;
+	adj.push_back(neighbor_list);
+	++size;
+}
+
+
+void Graph::pop_vertex(){
+	adj.pop_back();
+	--size;
+}
+
+
+
 void Graph::dfs(function<void(int)> discovery_action = [](int){}, function<void(int)> finish_action = [](int){}){
+	/* The recursive depth-first traversal algorithm for a possibly unconnected graph.
+ 	Iterates through the vector of ordered vertices and calls dfs whenever it finds a new component.
+	*/
 	vector<bool> vis(size);
 
 	for(int v = 1; v <= size; ++v)
 		if(!vis[v])
-			dfs_helper(v, vis, discovery_action, finish_action);
+			dfs_recursive(v, vis, discovery_action, finish_action);
 }
 
 
-// Basic dfs recursive function. Supports passing functional callbacks to execute at vertex discovery or finish.
-void Graph::dfs_helper(int v, vector<bool>& vis, function<void(int v)> discovery_action = [](int){}, function<void(int v)> finish_action = [](int){}){
+void Graph::dfs_recursive(int v, vector<bool>& vis, function<void(int v)> discovery_action = [](int){}, function<void(int v)> finish_action = [](int){}){
+	/* Basic depth-first search recursive function. Supports passing functional callbacks to execute at vertex discovery or finish.
+	Call this directly instead of Graph::dfs if you need to depth first search the graph in a custom order
+	*/
 	vis[v] = 1;
 	discovery_action(v);
 
 	for(auto e: adj[v])
 		if(!vis[e.vertex])
-			dfs_helper(e.vertex, vis, discovery_action, finish_action);
+			dfs_recursive(e.vertex, vis, discovery_action, finish_action);
+
 	finish_action(v);
 }
 
 
-// Bfs traversal algorithm.
-// As before we maintain a visits vector and a queue of the next vertices to visit.
 void Graph::bfs(int start_vertex){
+	/* Breadth-first search traversal algorithm.
+	*/
 	vector<bool> vis(size);
 	queue<int> q;
 
 	q.push(start_vertex);
 	vis[start_vertex] = 1;
 
-	bfs_helper(vis, q);
+	bfs_recursive(vis, q);
 }
 
 
-void Graph::bfs_helper(vector<bool>& vis, queue<int>& q){
-	int v = q.front(); //starting vertex
+void Graph::bfs_recursive(vector<bool>& vis, queue<int>& q){
+	int v = q.front();
 
-	for(auto e: adj[v]) //extend queue
+	for(auto e: adj[v])
 		if(!vis[e.vertex]){
 			q.push(e.vertex);
 			vis[e.vertex] = 1;
@@ -143,20 +167,24 @@ void Graph::bfs_helper(vector<bool>& vis, queue<int>& q){
 
 	q.pop(); 
 	if(!q.empty())
-		bfs_helper(vis, q);
+		bfs_recursive(vis, q);
 }
 
 
-// Strongly connected components. 
-// Uses slightly modified DFS, that builds up a vector of the vertices in reverse order of their finishing times.
-// Required by Kosaraju's algorithm for SCC. This ordering satisfies the property that the first occurences
-// of all SCCs form a topologicaly sorted subsequence.
-void Graph::strongly_connected_components(){
-	// Because DFS takes functions as parameters we do not have to implement it again
-	vector<int> order_for_scc;
-	dfs([](int){}, [&order_for_scc](int v){order_for_scc.push_back(v);});
+// Connectivity algorithms
 
-	Graph g_reversed = this->reverse_graph();
+
+vector<vector<int>> Graph::strongly_connected_components(){
+	/* Strongly connected components with Kosaraju's algorithm. 
+    Uses a slightly modified DFS, that builds up a vector of the vertices in reverse order of their finishing times.
+    This ordering satisfies the property that the subsequence of the first occurences of all SCCs is a topologicaly sorted subsequence.
+	Because our DFS implementation takes lambdas as parameters we do not have to implement it again.
+	*/
+	vector<int> order_for_scc;
+	vector<vector<int>> SCC;
+	dfs([](int){}, [&order_for_scc](int v){order_for_scc.push_back(v);}); //construct the ordering
+
+	Graph g_reversed = this->reversed_graph();
 	
 	vector<bool> vis(size);
 	for(auto it = order_for_scc.rbegin(); it != order_for_scc.rend(); ++it){
@@ -164,14 +192,17 @@ void Graph::strongly_connected_components(){
 		if(!vis[v]){
 			// we DFS traverse again. Each call of dfs fills out a SCC
 			SCC.push_back(vector<int> {});
-			g_reversed.dfs_helper(v, vis, [](int v){SCC.back().push_back(v);}); 
+			g_reversed.dfs_recursive(v, vis, [&](int v){SCC.back().push_back(v);}); 
 		}
 	}
+
+	return SCC;
 }
 
 
-// Reverse a graph
-Graph Graph::reverse_graph(){
+Graph Graph::reversed_graph(){
+	/* Returns a new graph with reversed edges
+	*/
 	Graph rg(size);
 
 	for(int v = 1; v <= size; ++v)
@@ -182,11 +213,14 @@ Graph Graph::reverse_graph(){
 }
 
 
-// Dijkstra's algorithm for shortest paths from a source
-// STL priority queues do not support easily deleting an element, so we skip deleting the elements.
-// Because pq is pushed to at most E times, where E is the number of edges, pq.size is always <= E.
-// The number of operations is thus O(E * log E) which equals O(E * log V) as E < V^2
+// Shortest path algorithms
+
+
 vector<int> Graph::shortest_paths_dijkstra(int source){
+	/* Dijkstra's algorithm for shortest paths from a source. Requires positive weights.
+	Because pq is pushed to at most E times, where E is the number of edges, pq.size is always <= E.
+	The number of operations is thus O(E * log E) which equals O(E * log V) as E < V^2vector<bool> vis(size + 1);
+	*/
 	vector<bool> vis(size + 1);
 	vector<int> distance(size + 1);
 	for(auto& d: distance)
@@ -216,9 +250,10 @@ vector<int> Graph::shortest_paths_dijkstra(int source){
 }
 
 
-// Computes all distances from source to vertices in the graph.
-// If it detects a negative cycle, the algorithm returns an empty vector of distances.
 vector<int> Graph::shortest_paths_bellman_ford(int source){
+	/* Bellman Ford shortest path algorithm.
+    If it detects a negative cycle, the algorithm returns an empty vector of distances.
+	*/
 	vector<bool> vis(size + 1);
 	vector<int> distance(size + 1);
 	for(auto& d: distance)
@@ -226,7 +261,7 @@ vector<int> Graph::shortest_paths_bellman_ford(int source){
 	distance[source] = 0;
 
 	for(int i = 1; i <= size - 1; ++i){// repeat |V| - 1 times
-		// Body of the for traverses all edges once and relaxes the distance vector.
+		// Body of the loop traverses all edges once and relaxes the distance vector.
 		for(int v = 1; v <= size; ++v)
 			for(auto edge: adj[v])
 				if(distance[edge.vertex] > distance[v] + edge.weight)
@@ -243,7 +278,7 @@ vector<int> Graph::shortest_paths_bellman_ford(int source){
 }
 
 
-Adj_Matrix all_shortest_paths(int n, Adj_Matrix M){
+Adj_Matrix all_shortest_paths_floyd_warshall(int n, Adj_Matrix M){
 	int dp[max_matrix][max_matrix][max_matrix];
 	Adj_Matrix ans;
 
@@ -261,6 +296,38 @@ Adj_Matrix all_shortest_paths(int n, Adj_Matrix M){
 			ans[i][j] = dp[i][j][n];
 
 	return ans;
+}
+
+
+vector<vector<int>> Graph::all_shortest_paths_johnsons(){
+	/*
+	Johnson's algorithm for all shortest paths. Returns a table all_paths such that all_paths[i][j] is the shortest
+	possible distance of a route from i to j.
+	*/
+	this->push_vertex(); //add a virtual vertex to compute reweightings
+	for(int i = 1; i < size; ++i)
+		this->add_edge(size, i, 0);
+
+	vector<int> h;
+
+	h = this->shortest_paths_bellman_ford(size);
+	this->pop_vertex();
+
+	Graph reweighted_g(size);
+
+	for(int i = 1; i <= size; ++i)
+		for(auto edge: adj[i])
+			reweighted_g.add_edge(i, edge.vertex, h[edge.vertex] - h[i] + edge.weight);
+			
+	vector<vector<int>> all_paths(size + 1);
+
+	for(int i = 1; i <= size; ++i){
+		all_paths[i] = reweighted_g.shortest_paths_dijkstra(i);
+		for(int j = 1; j <= size; ++j)
+				all_paths[i][j] += (h[i] - h[j]); // converting to the original weights
+	}
+
+	return all_paths;
 }
 
 
@@ -314,72 +381,65 @@ Graph random_graph(int size){
 	return g;
 }
 
-ifstream fin;
-ofstream fout;
-int ecount = 0;
-
 
 vector<int> Graph::eulerian_cycle(int m){
-	
 	// Return {-1} for non eulerian graphs.
 	for(int i = 1; i <= size; ++i)
 		if(sym_adj[i].size() & 1)
 			return vector<int> {-1};
 
-
 	vector<int> cycle;
 	vector<int> vis(m + 1);
-	int node;
-	stack<int> stk;
 	
+	euler_helper(1, vis, cycle);
+	return cycle;
+}
 
-	
-	stk.push(1);
 
-	while(!stk.empty()){
-		node = stk.top();
-		
+void Graph::euler_helper(int node, vector<int>& vis, vector<int>& cycle){
 
-		if(!sym_adj[node].empty()){
-			SymmetricEdge edge = sym_adj[node].back();
-			sym_adj[node].pop_back();
+	while(!sym_adj[node].empty()){
+		SymmetricEdge edge = sym_adj[node].back();
+		sym_adj[node].pop_back();
 
-			if(!vis[edge.id]){
-				vis[edge.id] = 1;
-				stk.push(edge.to);
-				cycle.push_back(edge.to);
-				cout << edge.to;
-			}
-		}
-		else{
-			
-			stk.pop();
+
+		if(!vis[edge.id]){
+			vis[edge.id] = 1;
+			euler_helper(edge.to, vis, cycle);
 		}
 	}
 
-	return cycle;
+	cycle.push_back(node);
 }
 
 
 int main(){
 	
 	srand(time(0));
-	fin.open("ciclueuler.in");
-	fout.open("ciclueuler.out"); 
+	ifstream fin;
+	ofstream fout;
+	fin.open("royfloyd.in");
+	fout.open("royfloyd.out");
 
-	
 	int n, m, u, v, weight;
-	fin >> n >> m;
+	fin >> n;
 	Graph g(n);
 
-	for(int i=1; i <= m; ++i){
-		fin >> u >> v;
-		g.add_symmetric_edge(u, v, i);
+	for(int i=1; i <= n; ++i)
+		for(int j=1; j <= n; ++j){
+			fin >> weight;
+			if(weight || i == j)
+				g.add_edge(i, j, weight);
+			else
+				g.add_edge(i, j, infty);
+		}
+
+	vector<vector<int>> all_paths = g.all_shortest_paths_johnsons();
+
+	for(int i=1; i <= n; ++i){
+		for(int j=1; j <= n; ++j){
+			fout << ((all_paths[i][j] >= infty) ? 0 : all_paths[i][j]) << " ";
+		}
+		fout << "\n";
 	}
-
-	vector<int> cycle;
-	cycle = g.eulerian_cycle(m);
-
-
- 
 }
